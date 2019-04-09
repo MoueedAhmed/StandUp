df02781ffb0af6d42f1084ff35cf0fbc41a98d39
Rao Moueed Ahmed <moueedrao@gmail.com>
2019-04-09 13:22:54 +0500

    Add: Set up the setOnCheckedChangeListener() method

--- app/src/main/java/com/amoueed/standup/MainActivity.java
+++ app/src/main/java/com/amoueed/standup/MainActivity.java
@@ -2,6 +2,9 @@ package com.amoueed.standup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ToggleButton alarmToggle = findViewById(R.id.alarmToggle);
+        alarmToggle.setOnCheckedChangeListener(
+                new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+
+                        String toastMessage;
+                        if(isChecked){
+                            //Set the toast message for the "on" case.
+                            toastMessage = "Stand Up Alarm On!";
+                        } else {
+                            //Set the toast message for the "off" case.
+                            toastMessage = "Stand Up Alarm Off!";
+                        }
+
+                        //Show a toast to say the alarm is turned on or off.
+                        Toast.makeText(MainActivity.this, toastMessage,Toast.LENGTH_SHORT).show();
+                    }
+                });
+
     }
 }
